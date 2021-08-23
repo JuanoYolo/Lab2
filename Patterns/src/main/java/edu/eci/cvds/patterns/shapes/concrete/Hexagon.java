c4f18d403226d32982d455483846afec68d63742
Juano <juanosierra2@gmail.com>
2021-08-23 18:24:57 -0500

    Se agrego shapeMain, se creo el archivo shapeFactory usando switch-case

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Hexagon.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Hexagon.java
@@ -2,7 +2,7 @@ package edu.eci.cvds.patterns.shapes.concrete;
 
 import edu.eci.cvds.patterns.shapes.Shape;
 
-public class Hexagon {
+public class Hexagon implements Shape {
     public int getNumberOfEdges() {
         return 6;
     }
