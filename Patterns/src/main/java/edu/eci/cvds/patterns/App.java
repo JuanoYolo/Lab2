a40a300465c72f175314d3ae4a678242823c0778
Juano <juanosierra2@gmail.com>
2021-08-23 17:54:00 -0500

    nuevo

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,18 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        String[] nombre = args;
+        String aux = "";
+        for(int i = 0; i<nombre.length; i++){
+            aux = aux+nombre[i]+" ";
+        }
+        System.out.println( "Hello "+aux+"!");
+    }
+}
